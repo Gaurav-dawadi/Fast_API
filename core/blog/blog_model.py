de04588afcc1172b5217ada382c94da09aace46a
Gaurav-dawadi <gaurabdawadi75@gmail.com>
2022-07-27 13:15:07 +0545

    refactor: change BlogModel's Base model

--- core/blog/blog_model.py
+++ core/blog/blog_model.py
@@ -1,14 +1,14 @@
-from ..base_model import Base
+from ..base_model import BaseModel
 from sqlalchemy import Column, ForeignKey, Integer, String
 # from sqlalchemy.orm import declarative_base, relationship
 
-
-class BlogModel(Base):
+class BlogModel(BaseModel):
     __tablename__ = "blogs"
+
     title = Column(String, index=True)
     description = Column(String, index=True)
-    # id = Column(Integer, primary_key=True, index=True)
     # owner_id = Column(Integer, ForeignKey("user.id"), nullable=False)
     # owner = relationship("User", back_populates="blogs")
+    
     def __repr__(self):
-        return f"Blog(id={self.id!r}, title={self.title!r})"
+        return f"Blog(title={self.title!r})"
